62cd9b33c8e773ad29b78f84fe69531f04156c71
George Ciurca <george.ciurca@gmail.com>
2021-12-18 09:11:19 +0000

    learning JS

---
+++ app.js
@@ -0,0 +1,10 @@
+/* console.dir(document);
+
+document.body.children[1].children[0].href = 'https://gnphotoart.co.uk'; */
+
+
+let anchorElement = document.getElementById('external-link');
+
+anchorElement.href = 'https://gnphotoart.co.uk';
+
+document.querySelector('#external-id');
