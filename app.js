af8254f33dbd6d8e16ea7a0589b7a31d0c1734bb
George Ciurca <george.ciurca@gmail.com>
2021-12-20 08:37:28 +0000

    learning in progress

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-/* console.dir(document);
+//ADD AN ELEMENT
+//1. Create the new element
 
-document.body.children[1].children[0].href = 'https://gnphotoart.co.uk'; */
+let newAnchorElement = document.createElement('a');
+newAnchorElement.href = 'https://gnphotoart.co.uk';
+newAnchorElement.textContent = 'This leads to my website';
 
+//2.Get access to the parent element that should hold the new element
 
-let anchorElement = document.getElementById('external-link');
+let firstParagraph = document.querySelector('p');
 
-anchorElement.href = 'https://gnphotoart.co.uk';
+//3.Insert the new element into the parent element content
 
-document.querySelector('#external-id');
+firstParagraph.append(newAnchorElement);
+
+//REMOVE ELEMENTS
+
+//1. Select the element that should be removed
+
+let firstH1Element = document.querySelector('h1');
+
+//2. Remove the element
+
+firstH1Element.remove();
+
+//MOVE ELEMENTS 
+firstParagraph.parentElement.append(firstParagraph);
+
+// innerHTML
+
+console.log(firstParagraph.innerHTML);
+firstParagraph.innerHTML = 'Hi this is <strong>important!</strong>';
